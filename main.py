5d997d9fe4cb995c4d84e2d183d8b9ff2e8822a0
S4sunny023 <srocks023@gmail.com>
2023-06-23 21:54:19 +0530

    Project done

---
+++ main.py
@@ -0,0 +1,13 @@
+from phonenumbers import timezone, geocoder , carrier
+import phonenumbers
+
+number = input("Enter your number with +__: ")
+phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_number(phone)
+car = carrier.name_for_number(phone, "en")
+reg = geocoder.description_for_number(phone,"en")
+
+print(phone)
+print(time)
+print(car)
+print(reg)
